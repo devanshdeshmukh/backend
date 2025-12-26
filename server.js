4d422ff2d6b3ddd9fce016dfba9070f3588601dd
devansh deshmukh <devanshd536@gmail.com>
2025-12-26 20:07:23 +0530

    mini project

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 const express = require('express');
 
-const app = express(); //server create ho jata hai..
+const app = express(); //server create ho jata hai
 
-app.get('/home',(req,res)=>{
-    res.send("welcome to home page")
-})
-
-app.get('/about', (req,res)=>{
-    res.send("welcome to about page")
-})
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
 })
