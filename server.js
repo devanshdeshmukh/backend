055fce86f22d8026386f8f8ee0b6fe842072b484
devansh deshmukh <devanshd536@gmail.com>
2025-12-28 17:10:05 +0530

    work

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express(); //server create ho jata hai
 
+app.use(express.json()) //this this the midlewere
+
+// notes => title and decription
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
+app.post('/notes',(req,res)=>{
+    const {title,content} = req.body
+    console.log(title,content)
+})
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
